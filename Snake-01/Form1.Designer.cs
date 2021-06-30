4b36d582ccc1e976326f67fe6ef98a6297ee65c5
Justin Villamor <justinvillamorhelp@gmail.com>
2021-06-30 03:42:26 -0400

    First Commit

--- Snake-01/Form1.Designer.cs
+++ Snake-01/Form1.Designer.cs
@@ -30,12 +30,120 @@ namespace Snake_01
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Player1 = new System.Windows.Forms.PictureBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.Player2 = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.Player1Score = new System.Windows.Forms.Label();
+            this.Player2Score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player1
+            // 
+            this.Player1.AccessibleName = "Player1";
+            this.Player1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Player1.Location = new System.Drawing.Point(12, 212);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(27, 127);
+            this.Player1.TabIndex = 0;
+            this.Player1.TabStop = false;
+            this.Player1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            // 
+            // Player2
+            // 
+            this.Player2.AccessibleName = "Player2";
+            this.Player2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Player2.Location = new System.Drawing.Point(889, 212);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(27, 127);
+            this.Player2.TabIndex = 1;
+            this.Player2.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.SystemColors.Window;
+            this.Ball.Location = new System.Drawing.Point(458, 261);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(20, 20);
+            this.Ball.TabIndex = 2;
+            this.Ball.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.timerTick);
+            // 
+            // Player1Score
+            // 
+            this.Player1Score.AutoSize = true;
+            this.Player1Score.Font = new System.Drawing.Font("Symbol", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Player1Score.ForeColor = System.Drawing.SystemColors.Window;
+            this.Player1Score.Location = new System.Drawing.Point(12, 9);
+            this.Player1Score.Name = "Player1Score";
+            this.Player1Score.Size = new System.Drawing.Size(39, 30);
+            this.Player1Score.TabIndex = 3;
+            this.Player1Score.Text = "00";
+            // 
+            // Player2Score
+            // 
+            this.Player2Score.AutoSize = true;
+            this.Player2Score.Font = new System.Drawing.Font("Symbol", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Player2Score.ForeColor = System.Drawing.SystemColors.Window;
+            this.Player2Score.Location = new System.Drawing.Point(877, 9);
+            this.Player2Score.Name = "Player2Score";
+            this.Player2Score.Size = new System.Drawing.Size(39, 30);
+            this.Player2Score.TabIndex = 4;
+            this.Player2Score.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(928, 574);
+            this.Controls.Add(this.Player2Score);
+            this.Controls.Add(this.Player1Score);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.Player1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(944, 613);
+            this.MinimumSize = new System.Drawing.Size(944, 613);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keydown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyup);
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Player1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label Player2Score;
+        private System.Windows.Forms.Label Player1Score;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.PictureBox Player2;
+        private System.Windows.Forms.Timer GameTimer;
     }
 }
 
--- Snake-01/Form1.cs
+++ Snake-01/Form1.cs
@@ -6,16 +6,157 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Snake_01
 {
     public partial class Form1 : Form
     {
+        bool  p1_w,p1_s,p2_w, p2_s, winner;// boolean to be used to detect player position
+        int speed = 10; 
+        int ballx = 8, bally = 8; // horizontal and vertical speed value for the ball object  
+        int p1score = 0, p2score = 0, maxscore = 5;
+        string endmessage = "Would you to play again?";
+        string title = "Restart?";
+        MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //Init
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+
+        }
+
+        private void keydown(object sender, KeyEventArgs e)
+        {
+
+            //Player 1 Movement Booleans
+            if (e.KeyCode == Keys.W)
+                p1_w = true;
+            if (e.KeyCode == Keys.S)
+                p1_s = true;
+            //Player 2 Movement Booleans
+            if (e.KeyCode == Keys.Up)
+                p2_w = true;
+            if (e.KeyCode == Keys.Down)
+                p2_s = true;
+
+            //End Program on ESC Key
+            if (e.KeyCode == Keys.Escape)
+                this.Close();
+        }
+
+        private void keyup(object sender, KeyEventArgs e)
+        {
+            //Player 1 Movement Booleans
+            if (e.KeyCode == Keys.W)
+                p1_w = false;
+            if (e.KeyCode == Keys.S)
+                p1_s = false;
+
+            //Player 2 Movement Booleans
+            if (e.KeyCode == Keys.Up)
+                p2_w = false;
+            if (e.KeyCode == Keys.Down)
+                p2_s = false;
+        }
+        //Game Loop
+        private void timerTick(object sender, EventArgs e)
+         {
+            Player1Score.Text = "" + p1score;
+            Player2Score.Text = "" + p2score;
+
+            Ball.Top -= bally;
+            Ball.Left -= ballx;
+
+
+            if(p1score < maxscore)
+            {
+                if(p2score < maxscore)
+                {
+                    //If player two scores
+                    if(Ball.Left < 0) //If Ball Went past player 1(left)
+                    {
+                        Ball.Left = 434;//Set Ball To Center of the screen
+                        ballx = -ballx;
+                        ballx += 2;
+                        p2score++;
+                    }
+                    //If player one scores
+                    if (Ball.Left + Ball.Width > ClientSize.Width) //If Ball Went past player 1(left)
+                    {
+                        Ball.Left = 434;//Set Ball To Center of the screen
+                        ballx = -ballx;
+                        ballx += 3;
+                        p1score++;
+                    }
+
+                    if (Ball.Top < 0 || Ball.Top + Ball.Height > ClientSize.Height)
+                    {
+                        bally = -bally;
+                    }
+
+
+                    //Player Movement
+                    if (p1_w == true && Player1.Top > 0)
+                        Player1.Top -= speed;
+                    if (p1_s == true && Player1.Top < 455)
+                        Player1.Top += speed;
+
+                    if (p2_w == true && Player2.Top > 0)
+                        Player2.Top -= speed;
+                    if (p2_s == true && Player2.Top < 455)
+                        Player2.Top += speed;
+
+                    //Boundary Check
+                    if (Ball.Bounds.IntersectsWith(Player1.Bounds) || Ball.Bounds.IntersectsWith(Player2.Bounds))
+                        ballx = -ballx;
+                }
+            }
+            if(p1score == maxscore || p2score == maxscore)
+            {
+                if (p1score > p2score)
+                    winner = true;
+                else
+                    winner = false;
+
+                GameTimer.Stop();
+                if (winner)
+                    MessageBox.Show("Congrats Player 1!");
+                else
+                    MessageBox.Show("Congrats Player 2!");
+                DialogResult result = MessageBox.Show(endmessage, title, buttons);
+                if (result == DialogResult.No)
+                    this.Close();
+                else
+                    RestartGame();
+            }
+        }
+
+        private void RestartGame()
+        {
+            p1score = 0;
+            p2score = 0;
+            Ball.Left = 434;//Set Ball To Center of the screen
+            ballx = -ballx;
+            ballx += 3;
+            GameTimer.Start();
+        }
     }
 }
